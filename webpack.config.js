fed4da652b40b5d40dd1f67b5aa952cd077f6e47
Oleg Lukashenko <oleg.lukashenko.dev@gmail.com>
2020-12-02 09:49:22 +0300

    some changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,88 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 
-console.log('path', path.resolve(__dirname, 'src'))
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+
+  return config
+}
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  devtool: 'source-map',
+  devtool: isDev ? 'cheap-module-source-map' : false,
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 3000
+  },
+  optimization: optimization(),
   entry: {
-    'pdf1': './pdf1/main.js',
-    // 'pdf2': './pdf2/main.js'
+    'exp1': './exp1/index.js',
+    // 'exp2': './exp2/main.js'
   },
   output: {
-    // path: __dirname,
-    // filename: "build/[name]/bundle.js"
+    publicPath: '',
     filename: '[name]/[hash:20].js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'build'),
   },
   module: {
     rules: [
       {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
         test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader'
       }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '/pdf1/index.html'),
-      chunks: ['page1'],
-      filename: 'pdf1/index.html'
+    new MiniCssExtractPlugin({
+      filename: '[name]/[name].css',
+      ignoreOrder: false,
     }),
-    // new HtmlWebpackPlugin({
-    //   template: path.resolve(__dirname, '/pdf2/index.html'),
-    //   chunks: ['page2'],
-    //   filename: 'pdf2/index.html'
-    // }),
-
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '/exp1/index.html'),
+      filename: 'exp1/index.html',
+      inject: true,
+    }),
   ]
 };
